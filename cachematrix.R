b08856ead938ead890b580832022131b6e238fb6
Anne Domnic <renoretu@gmail.com>
2015-03-21 10:56:05 -0400

    Add cachematrix.R file

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,59 @@
-## Put comments here that give an overall description of what your
-## functions do
+  ## Assignment: Caching the Inverse of a Matrix
+  ## Computing the inverse of a square matrix
 
-## Write a short comment describing this function
+  ## Caching the inverse of a Matrix and to call
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(x = numeric()) {
+  
+  ## holds the cached value or NULL if nothing is cached 
+  ## initially nothing is cached so set it to NULL 
+      cache <- NULL 
+           
+  ## store a matrix 
+      setMatrix <- function(newValue) { 
+      x <<- newValue 
+  ## since the matrix is assigned a new value, flush the cache 
+      cache <<- NULL 
+  } 
+  
+  ## returns the stored matrix 
+    getMatrix <- function() { 
+      x 
+  } 
+   
+  ## cache the given argument  
+    cacheInverse <- function(solve) { 
+      cache <<- solve 
+  } 
+   
+  ## get the cached value 
+    getInverse <- function() { 
+      cache 
+  } 
+           
+  ## return a list. Each named element of the list is a function 
+    list(setMatrix = setMatrix, getMatrix = getMatrix, cacheInverse = cacheInverse, getInverse = getInverse) 
+  } 
+ 
+   
+  ## The following function calculates the inverse of a "special" matrix created with  
+  ## makeCacheMatrix 
+  cacheSolve <- function(y, ...) { 
+  
+    ## get the cached value 
+      inverse <- y$getInverse() 
+    ## if a cached value exists return it 
+      if(!is.null(inverse)) { 
+      message("getting cached data") 
+        return(inverse) 
+  } 
+  ## otherwise get the matrix, caclulate the inverse and store it in 
+  ## the cache 
+      data <- y$getMatrix() 
+    inverse <- solve(data) 
+    y$cacheInverse(inverse) 
+           
+  ## return the inverse 
+     inverse 
+  } 
 
-}
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
